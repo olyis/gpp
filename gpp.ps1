9b2b2152cc38ac87dc555b5c33e3921a5a71bbbd
Oliver Sourbut (RFS/RCS <oliver.sourbut@towerswatson.com>
2017-05-09 11:57:03 +0100

    Fix slash direction in nested branch names

--- gpp.ps1
+++ gpp.ps1
@@ -40,7 +40,7 @@ function Get-Branch {
     if (Test-Path $branchesPath) {
         Get-ChildItem -Path $branchesPath -Recurse -File |
             % {@{
-                    Name = cutPrefix -prefix $branchesPath -target $_.FullName
+                    Name = (cutPrefix -prefix $branchesPath -target $_.FullName).Replace('\', '/')
                     Target = Get-Content -Path $_.FullName
                 }}
     }
@@ -49,23 +49,23 @@ function Get-Branch {
 function Checkout-Branch {
     [CmdletBinding()]
     Param(
+        [Parameter(position=1)]
         [switch] $Remote
     )
 
     DynamicParam {
-        # Retrieve local branch names
-        # TODO retrieve remotes or all based on optional flag param?
+        # Retrieve branch names
         $names = Get-Branch -Remote:$Remote | % { $_.Name }
 
         # Define -Name parameter
         $nameAtts = New-Object System.Management.Automation.ParameterAttribute
         $nameAtts.HelpMessage = "Branch to checkout: $names"
         $nameAtts.Mandatory = $true
-        $nameAtts.Position = 1
+        $nameAtts.Position = 0
         $nameValidation = New-Object System.Management.Automation.ValidateSetAttribute($names)
         $nameParam = New-Object System.Management.Automation.RuntimeDefinedParameter('Name', [String], @($nameAtts, $nameValidation))
 
-        # dynamic parameters to return
+        # Dynamic parameters to return
         $dynamicParams = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
         $dynamicParams.Add('Name', $nameParam)
         return $dynamicParams
